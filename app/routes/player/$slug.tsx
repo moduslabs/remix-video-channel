4d49ec27041fd62eadbee8ef0b9149d40b98f4f2
Aaron-Goldman <aaron.goldman@moduscreate.com>
2022-02-25 15:33:38 -0500

    add like and comment counts

--- app/routes/player/$slug.tsx
+++ app/routes/player/$slug.tsx
@@ -1,32 +1,38 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { Link, Outlet, useLoaderData } from "remix";
 import type { LoaderFunction } from "remix";
 import invariant from "tiny-invariant";
+import { getVideoData } from "~/videoData";
+import { youtube_v3 } from "@googleapis/youtube";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, "expected params.slug");
-  return params.slug;
+  const videoData = await getVideoData(params.slug);
+
+  return videoData;
 };
 
 export default function Player() {
-  const id = useLoaderData();
+  const { id, player, statistics } = useLoaderData<youtube_v3.Schema$Video>();
 
   return (
     <Grid container>
       <Grid item xs={12}>
         <iframe
-          id="ytplayer"
           width="640"
           height="360"
           src={`https://www.youtube.com/embed/${id}?autoplay=1`}
           frameBorder="0"
         ></iframe>
       </Grid>
-      <Grid item xs={8}></Grid>
+      <Grid item xs={8}>
+        <Typography variant="h6">{`${statistics?.likeCount} likes`}</Typography>
+      </Grid>
       <Grid item xs={4}>
-        <Link to={`/player/${id}/comments`}>
-          <Button>Comments</Button>
-        </Link>
+        <Button
+          component={Link}
+          to={`/player/${id}/comments`}
+        >{`${statistics?.commentCount} Comments`}</Button>
       </Grid>
       <Outlet />
     </Grid>
