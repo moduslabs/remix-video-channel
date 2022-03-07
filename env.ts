e32252cfb64ea71c14b9db2bb3716d3c17147184
Grgur Grisogono <grgur@grgur.com>
2022-03-07 19:46:58 +0100

    feat: custom env

---
+++ env.ts
@@ -0,0 +1,7 @@
+  
+const config = {
+  NODE_ENV: `development`,
+  YT_API_KEY: `AIzaSyC9OK6hdQcCU6EpK34DrbGHkbe-ODRrBBE`
+};
+
+export default config;
--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
+
 module.exports = {
   serverBuildTarget: 'cloudflare-pages',
   server: './server.js',
