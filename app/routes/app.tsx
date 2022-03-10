054ac3ccd7ee9b5c825881ff96a04fbee966aeaf
Aaron-Goldman <aaron.goldman@moduscreate.com>
2022-03-10 17:48:55 -0500

    add more playlists

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -10,6 +10,18 @@ export default function Playlist() {
       title: "Recent",
       id: "UUsKwL0-e2eHRNa6Ne99AESw",
     },
+    {
+      title: "Vue 3",
+      id: "PLMLZt4pr7Aq6AfC_ynfeDbEk2hbMFGpHO",
+    },
+    {
+      title: "Atlassian",
+      id: "PLMLZt4pr7Aq7T8rQBEfHYIPFPCKllVjB6",
+    },
+    {
+      title: "React",
+      id: "PLMLZt4pr7Aq5BiAXhNXexzH6UYLtGKhnr",
+    },
   ];
 
   return (
