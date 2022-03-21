30d50d8dd4514ce2a0da7cce90dfd5d6f63a733a
Grgur Grisogono <grgur@grgur.com>
2022-03-21 15:46:47 +0100

    fix: tailwind build

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
-import styles from "../styles/app.css";
+import styles from "./styles/app.css";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
