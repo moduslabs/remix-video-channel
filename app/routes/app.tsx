b352422b2c36467740593a4ba5677466af8e591b
Grgur Grisogono <grgur@grgur.com>
2022-03-22 16:09:13 +0100

    fix: responsiveness

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,11 +1,11 @@
-import { NavLink, Outlet } from "remix";
-import Search from "./Search";
-import { playlists } from "~/data/playlists";
+import { NavLink, Outlet } from 'remix';
+import Search from './Search';
+import { playlists } from '~/data/playlists';
 
 export default function Playlist() {
   return (
     <div>
-      <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+      <div className="w-32 flex md:w-64 flex-col fixed inset-y-0">
         <div className="flex-1 flex flex-col min-h-0 bg-gray-700">
           <div className="mt-5 flex-1 h-0 overflow-y-auto">
             <nav className="px-2 space-y-1">
@@ -16,8 +16,8 @@ export default function Playlist() {
                   to={`./${id}`}
                   className={({ isActive }) =>
                     isActive
-                      ? "group flex items-center px-2 py-2 text-white font-medium rounded-md bg-gray-900"
-                      : "group flex items-center px-2 py-2 text-white font-medium rounded-md"
+                      ? 'group flex items-center px-2 py-2 text-white font-medium rounded-md bg-gray-900'
+                      : 'group flex items-center px-2 py-2 text-white font-medium rounded-md'
                   }
                 >
                   {title}
@@ -28,7 +28,7 @@ export default function Playlist() {
         </div>
       </div>
 
-      <div className="md:pl-64 flex flex-col flex-1">
+      <div className="pl-32 md:pl-64 flex flex-col flex-1">
         <main className="flex-1">
           <Outlet />
         </main>
--- app/routes/app/$playlist.tsx
+++ app/routes/app/$playlist.tsx
@@ -1,22 +1,16 @@
-import {
-  Link,
-  LoaderFunction,
-  Outlet,
-  useLoaderData,
-  useLocation,
-} from "remix";
-import invariant from "tiny-invariant";
+import { Link, LoaderFunction, Outlet, useLoaderData, useLocation } from 'remix';
+import invariant from 'tiny-invariant';
 import {
   GenericYouTubeVideoListItem,
   getPlaylistItems,
   getSearchResults,
-} from "~/data/playlistItems";
+} from '~/data/playlistItems';
 
 export const loader: LoaderFunction = async ({ params, request, context }) => {
-  invariant(params.playlist, "expected params.playlist");
-  if (params.playlist === "search") {
+  invariant(params.playlist, 'expected params.playlist');
+  if (params.playlist === 'search') {
     const url = new URL(request.url);
-    const query = url.searchParams.get("query") as string;
+    const query = url.searchParams.get('query') as string;
     const searchResults = await getSearchResults(query, context);
 
     return searchResults;
@@ -33,22 +27,18 @@ export default function Playlist() {
   return (
     <div className="flex flex-col h-screen divide-y items-center">
       <Outlet />
-      <div className="flex-1 grow overflow-y-auto p-4">
-        <ul className="grid grid-cols-1 gap-2 sm:grid-cols-2">
+      <div className="flex-1 grow overflow-y-auto p-1 sm:p-2 md:p-4">
+        <ul className="grid grid-cols-1 gap-2 lg:grid-cols-2">
           {videos.map(({ description, title, thumbnail, id }) => (
             <li
               key={id}
-              className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
+              className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 px-1"
             >
               <Link to={{ pathname: `./${id}`, search: search }}>
-                <div className="w-full flex items-center justify-between">
-                  <img
-                    src={thumbnail}
-                    alt={title}
-                    className="w-32 h-18 rounded-l-lg"
-                  />
-                  <div className="flex-1 p-2 truncate">
-                    <p className="text-gray-900 text-sm truncate">{title}</p>
+                <div className="md:w-full flex md:flex-row flex-col items-center justify-between">
+                  <img src={thumbnail} alt={title} className="w-32 h-18 rounded-l-lg" />
+                  <div className="flex-1 p-2 md:truncate">
+                    <p className="text-gray-900 text-sm md:truncate">{title}</p>
                     <p className="text-gray-700 text-xs line-clamp-2 whitespace-normal">
                       {description}
                     </p>
